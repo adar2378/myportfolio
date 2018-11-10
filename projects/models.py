a01f5d72d36bcddb09131ba7aca704da9f9cb966
adar2378 <saifulislamadar@gmail.com>
2018-11-10 22:36:41 +0600

    commiting agian

--- projects/models.py
+++ projects/models.py
@@ -6,3 +6,12 @@ from django.db import models
 class Projects(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+    title = models.CharField(max_length=100, default='')
+    githubLink = models.CharField(max_length=200, default=' ')
+    myLink = "https://github.com/adar2378/"
+
+    def getLink(self):
+        return self.myLink+self.githubLink
+
+    def __str__(self):
+        return self.title
